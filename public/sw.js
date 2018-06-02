09188b6e25df152fdf27044c6941ac72dbf160a7
tomvaneygen <tom.van.eygen@student.howest.be>
2018-06-02 12:06:43 +0200

    no caching

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+// importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
@@ -6,7 +6,7 @@ if (workbox) {
   console.log(`Boo! Workbox didn't load 😬`);
 }
 
-workbox.routing.registerRoute(
-  new RegExp('.*\.js'),
-  workbox.strategies.cacheFirst(),
-);
+// workbox.routing.registerRoute(
+//   new RegExp('.*\.js'),
+//   workbox.strategies.cacheFirst(),
+// );
